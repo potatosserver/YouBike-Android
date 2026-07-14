a8db2e719d6ba0ebd6a19308e350bc41b616f05d
potatosserver <potatosserver@gmail.com>
2026-07-14 22:26:39 +0800

    cleanup: remove dead code and obsolete L10n extension

--- lib/extensions/l10n_extension.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:youbike_android/l10n/app_localizations.dart';
-
-extension L10nExtension on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
-}
