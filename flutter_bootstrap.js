eea018ecb529f88890b6115c83106b9db39f2538
potatosserver <potatosserver@users.noreply.github.com>
2026-07-20 12:45:11 +0000

    Deploying to gh-pages from @ potatosserver/YouBike-Flutter@3e770bfc9ca67e02b35a9ad2123e80feeac01ae0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3940117203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1610361350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
